37395a828f427447414066d786c07dfcd0445093
Valiantsin Ushkevich <vushkevih@gmail.com>
2025-10-28 21:12:03 +0100

    first commit

---
+++ src/main/java/com/project/gamingplatform/GamingplatformApplication.java
@@ -0,0 +1,13 @@
+package com.project.gamingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingplatformApplication.class, args);
+	}
+
+}
