d7b5b0d282f0803cafabaf8e48bfac8fd8d7d1d6
Valiantsin Ushkevich <vushkevih@gmail.com>
2025-11-14 22:24:30 +0100

    Custom Authentication Success Handler is finished

---
+++ src/main/java/com/project/gamingplatform/config/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,30 @@
+package com.project.gamingplatform.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String username = userDetails.getUsername();
+        System.out.println("User: " + username + " is logged in.");
+        boolean hasAdminRole = authentication.getAuthorities().stream().allMatch( a -> a
+                .getAuthority().equals("ROLE_ADMIN"));
+        boolean hasPlayerRole = authentication.getAuthorities().stream().allMatch( p -> p
+                .getAuthority().equals("ROLE_PLAYER"));
+        if(hasAdminRole || hasPlayerRole){
+            response.sendRedirect("/dashboard");
+        }
+    }
+}
--- src/main/java/com/project/gamingplatform/config/WebSecurityConfig.java
+++ src/main/java/com/project/gamingplatform/config/WebSecurityConfig.java
@@ -1,7 +1,12 @@
 package com.project.gamingplatform.config;
 
+import com.project.gamingplatform.service.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,20 +18,39 @@ public class WebSecurityConfig {
             "/login", "/register", "/register/**", "/webjars/**", "/resources/**", "/assets/**",
             "/css/**", "/summernote/**", "/js/**", "/*.css", "/*.js", "/*.js.map", "/fonts**",
             "/favicon.ico", "/resources/**", "/error"};
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+    private final CustomUserDetailService customUserDetailService;
+
+    public WebSecurityConfig(CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler, CustomUserDetailService customUserDetailService) {
+        this.customAuthenticationSuccessHandler = customAuthenticationSuccessHandler;
+        this.customUserDetailService = customUserDetailService;
+    }
 
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity.authenticationProvider(authenticationProvider());
+
         httpSecurity
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers(publicUrl).permitAll();
                     auth.anyRequest().authenticated();
                 });
-
-        httpSecurity.formLogin(form -> form
+        httpSecurity.formLogin(login -> login
                         .loginPage("/login").permitAll()
-                        .defaultSuccessUrl("/", true)
-                );
+                        .successHandler(customAuthenticationSuccessHandler)) // !!!!!!!
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                    .logoutSuccessUrl("/")
+                ).cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable());
         return httpSecurity.build();
+    }
+
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(customUserDetailService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
     }
 
 //    @Bean
