b2ca7527839c2f2626fdad3ff3c0ea1fdb4fc9a0
Valiantsin Ushkevich <vushkevih@gmail.com>
2025-11-16 17:58:42 +0100

    feat: logout is added

--- src/main/java/com/project/gamingplatform/controller/UsersController.java
+++ src/main/java/com/project/gamingplatform/controller/UsersController.java
@@ -2,8 +2,13 @@ package com.project.gamingplatform.controller;
 
 import com.project.gamingplatform.entity.Users;
 import com.project.gamingplatform.service.UsersService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,6 +43,15 @@ public class UsersController {
     public String login(){
         System.out.println("user start login");
         return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication != null){
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/";
     }
 
 
