6f60866dc557e6853a21df8a51438dab721a1213
Valiantsin Ushkevich <vushkevih@gmail.com>
2026-01-27 20:12:38 +0100

    ref: GameRoomService.java : 	1) method saveGameRoom has been changed: 		the method of receiving the user ID has been changei. 	2) name of class has been changed.

--- src/main/java/com/project/gamingplatform/service/GameRoomsService.java
+++ src/main/java/com/project/gamingplatform/service/GameRoomsService.java
@@ -5,18 +5,19 @@ import com.project.gamingplatform.entity.GameRooms;
 import com.project.gamingplatform.entity.Users;
 import com.project.gamingplatform.repository.GameRoomsRepository;
 import com.project.gamingplatform.repository.UsersRepository;
+import com.project.gamingplatform.util.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class RoomService {
+public class GameRoomsService {
     private final GameRoomsRepository gameRoomsRepository;
     private final UsersRepository usersRepository;
 
     @Autowired
-    public RoomService(GameRoomsRepository gameRoomsRepository, UsersRepository usersRepository) {
+    public GameRoomsService(GameRoomsRepository gameRoomsRepository, UsersRepository usersRepository) {
         this.gameRoomsRepository = gameRoomsRepository;
         this.usersRepository = usersRepository;
     }
@@ -25,8 +26,10 @@ public class RoomService {
         GameRooms gameRoom = new GameRooms();
 
         Authentication authentication =  SecurityContextHolder.getContext().getAuthentication();
-        Users users = usersRepository.findByUsername(authentication.getName())
-                .orElseThrow(() -> new RuntimeException("User not found"));
+//        Users users = usersRepository.findByUsername(authentication.getName())
+//                .orElseThrow(() -> new RuntimeException("User not found"));
+        CustomUserDetails userDetail = (CustomUserDetails) authentication.getPrincipal();
+        Users users = userDetail.getUser();
 
         gameRoom.setCreatedBy(users);
         gameRoom.setRoomName(room.getRoomName());
